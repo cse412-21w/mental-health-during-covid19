35d0482c5b17714910400d2f51f1ca00c94dc3e5
Travis Xie <yinuox@uw.edu>
2021-03-09 17:46:01 -0800

    Add mental health 2019

--- testsrc/mental_health_2019.js
+++ testsrc/mental_health_2019.js
@@ -2,6 +2,9 @@ import mhdata from '../static/mental_health_2019.csv'    // import dataset
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
 
+var races = ['Hispanic or Latino', 'Non-Hispanic white, single race', 'Non-Hispanic black, single race',
+'Non-Hispanic Asian, single race', 'Non-Hispanic, other races and multiple races']; 
+
 const options = {
   config: {
     // Vega-Lite default configuration
@@ -23,7 +26,7 @@ const options = {
 vl.register(vega, vegaLite, options);
 
 var dataArray = []
-
+/*
 d3.csv(mhdata).then(function(data) {
   data.forEach(function(d){
     var type = d.type;
@@ -32,25 +35,27 @@ d3.csv(mhdata).then(function(data) {
     dataArray.push({type: type, race: race, percentage: percentage})
   })
   drawLineVegaLite();
-});
-
+});*/
+drawLineVegaLite();
 function drawLineVegaLite() {
 
   vl.markBar()
-    .data(dataArray)
+    .data(mhdata)
     .encode(
-        vl.column().fieldN('type').spacing(10),
+        vl.column().fieldN('type').spacing(10).title(null),
         vl.y().fieldQ('percentage'),
-        vl.x().fieldN('race'),
+        vl.x().fieldN('race')
+          .sort(vl.fieldQ('percentage'))
+          .axis({title: null}),
         vl.color().fieldN('race'),
         vl.tooltip([vl.fieldN('race'), vl.fieldQ('percentage')])
     )
-    .width(50)
-    .height(450)
+    .width(100)
+    .height(400)
     .render()
     .then(viewElement => {
         // render returns a promise to a DOM element containing the chart
         // viewElement.value contains the Vega View object instance
-        document.getElementById('md_2019').appendChild(viewElement);
+        document.getElementById('md2019').appendChild(viewElement);
     })
 }
