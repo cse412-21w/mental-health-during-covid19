d68670d1010ca6ab59d251abe68eabd31a9dc1a1
Yichi Zhang <arthur233333@gmail.com>
2021-03-15 15:23:01 -0700

    fix map

--- src/javasript/covidMap.js
+++ src/javasript/covidMap.js
@@ -1,5 +1,6 @@
 import Covid_2019_Data from '../../static/adult_prevalance_2019.csv';    // import dataset
 import Covid_2020_Data from '../../static/adult_mh_prevalance_2020 (2).csv'; 
+import us_json from '../../static/us.json'
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
 
@@ -30,7 +31,7 @@ function drawLineVegaLite() {
     const Covid2019 = vl.layer (
         vl.markGeoshape({stroke: '#aaa', strokeWidth: 0.25})
         .title("Percentage of Adult with any Mental illness by State in 2019")
-        .data(vl.topojson('data/us-10m.json').feature('states'))
+        .data(vl.topojson(us_json).feature('states'))
         .transform(
           vl.lookup('id').from(vl.data(Covid_2019_Data).key('id').fields(['state', 'rank', 'percentage', 'number']))
         )
@@ -46,7 +47,7 @@ function drawLineVegaLite() {
     const Covid2020 = vl.layer (
         vl.markGeoshape({stroke: '#aaa', strokeWidth: 0.25})
         .title("Percentage of Adult with any Mental illness by State in 2020")
-        .data(vl.topojson('data/us-10m.json').feature('states'))
+        .data(vl.topojson(us_json).feature('states'))
         .transform(
           vl.lookup('id').from(vl.data(Covid_2020_Data).key('id ').fields(['state', 'rank', 'percentage', 'number']))
         )
